cbb074f9a8f718f0404897bd7750d1c48b352213
matafokka <da.real.matafokka@gmail.com>
2021-12-06 16:56:55 +0300

    Leaflet.Draw fixes - Bumped ALS version which brings goodies to L.Draw support - Fixed objects not drawing - Bumped project version

--- main.js
+++ main.js
@@ -8,11 +8,10 @@
 // I've spend hours struggling with this issue, so don't try to reorganise the code, you'll fail and, as it seems, break compatibility with the older Electron versions.
 
 //require("fastestsmallesttextencoderdecoder");
+require("leaflet-draw");
 require("leaflet-advanced-layer-system");
 L.ALS.Locales.AdditionalLocales.Russian();
 require("./node_modules/leaflet.coordinates/dist/Leaflet.Coordinates-0.1.5.min.js");
-require("leaflet-draw");
-window.type = true; // Fixes L.Draw error. Devs forgot to initialize this variable in their code, and project looks dead, so there will be no fixes from devs.
 require("./locales/English.js");
 require("./locales/Russian.js");
 require("./SynthGeometryLayer/SynthGeometryLayer.js");
